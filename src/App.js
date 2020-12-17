4e1adedbabf73e4286cf73744bf1fec9fbbb6902
Richard Cho <richardcho100@gmail.com>
2020-12-16 19:03:34 -0500

    Add title & search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className='search-form'>
+        <input className='search-bar'type='text'/>
+        <button className='search-button' type='submit'>Search</button>
+      </form>
     </div>
   );
 }
